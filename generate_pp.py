736daa3f6cc08b7c49ebd3f107e25772de2e5201
3MA1L <105881496+3MA1L@users.noreply.github.com>
2023-07-27 21:19:03 -0500

    Update generate_pp.py

--- generate_pp.py
+++ generate_pp.py
@@ -26,24 +26,7 @@ def create_powerpoint_slide(prs, slide_layout, placeholders):
             if shape.has_text_frame and placeholder in shape.text_frame.text:
                 shape.text_frame.text = text
 
-#def create_bar_chart(prs, incidents_last_call, medium_incidents_last_call, low_incidents_last_call,
-                     # incidents_current_call, medium_incidents_current_call, low_incidents_current_call):
-    # slide_layout = prs.slide_layouts[5]  # Use index 5 for the title and content slide layout with a chart
-    # slide = prs.slides.add_slide(slide_layout)
 
-    # x, y, cx, cy = Inches(2), Inches(2), Inches(6), Inches(4.5)
-    # chart = slide.shapes.add_chart(
-        # XL_CHART_TYPE.BAR_CLUSTERED, x, y, cx, cy
-    # ).chart
-
-    #Add chart data
-    # chart_data = CategoryChartData()
-    # chart_data.categories = ['Incidents', 'Medium Incidents', 'Low Incidents']
-    # chart_data.add_series('Last Call', (incidents_last_call, medium_incidents_last_call, low_incidents_last_call))
-    # chart_data.add_series('Current Call', (incidents_current_call, medium_incidents_current_call, low_incidents_current_call))
-
-    # chart.replace_data(chart_data)
-####
 def save_powerpoint_as_pdf(prs, output_filename):
     pdf_export_filename = output_filename.replace(".pptx", ".pdf")
     powerpoint = CreateObject("Powerpoint.Application")
@@ -54,7 +37,7 @@ def save_powerpoint_as_pdf(prs, output_filename):
     powerpoint.Quit()
     
 def set_named_placeholder(slide, shape_name, new_value):
-    #Find the shape byt its name in the slide shapes
+    #Find the shape by its name in the slide shapes
     for shape in slide.shapes:
         if shape.name == shape_name:
             for paragrap in shape.text_frame.paragraphs:
@@ -66,19 +49,19 @@ def set_named_placeholder(slide, shape_name, new_value):
 def main():
     # Load the PowerPoint template
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    template_filename = os.path.join(script_dir, "SecuritySlides.pptx")
+    template_filename = os.path.join(script_dir, "Sample.pptx")
     
     prs = Presentation(template_filename)
     
     slide_master = prs.slide_master
 
-    # Slide 1: Ask for customer name and update the template
+    # Slide 1: Ask for subject and update the template
 
-    customer_name = input("Enter Customer Name: ")
+    subject_one = input("Enter subject: ")
     placeholders_slide1 = {
-        'CUSTOMER_NAME': customer_name
+        'SUBJECT_ONE': subject_one
     }
-    set_named_placeholder(slide_master, "CUSTOMER_NAME", customer_name)
+    set_named_placeholder(slide_master, "SUBJECT_ONE", subject_one)
     update_placeholders(prs, placeholders_slide1)
 ###########################################################################################################################################
     # Slide 2: Ask for follow-up items and update the template
@@ -102,22 +85,22 @@ def main():
     set_named_placeholder(slide_master, 'FOLLOW_UP_ITEMS', follow_up)
     update_placeholders(prs, placeholders_slide2)
     ########################################################################################################################################
-    # Slide 3: Ask for incident details and update the template
-    incidents_last_call = int(input("Enter the number of Incidents for Last Call: "))
-    medium_incidents_last_call = int(input("Enter the number of Medium Incidents for Last Call: "))
-    low_incidents_last_call = int(input("Enter the number of Low Incidents for Last Call: "))
+    # Slide 3: Ask for current details and update the template
+    old_matters = int(input("Enter the number of matters for last subject: "))
+    old_other_matters = int(input("Enter the number of other matters for last subject: "))
+    old_other_other_matters = int(input("Enter the number of other other matters for last subject: "))
 
-    incidents_current_call = int(input("Enter the number of Incidents for Current Call: "))
-    medium_incidents_current_call = int(input("Enter the number of Medium Incidents for Current Call: "))
-    low_incidents_current_call = int(input("Enter the number of Low Incidents for Current Call: "))
+    current_matters = int(input("Enter the number of current matters: "))
+    other_current_matters = int(input("Enter the number of other current matters: "))
+    other_other_current_matters = int(input("Enter the number of other other current matters: "))
 
     placeholders_slide3 = {
-        'INCIDENTS_LAST_CALL': str(incidents_last_call),
-        'MEDIUM_INCIDENTS_LAST_CALL': str(medium_incidents_last_call),
-        'LOW_INCIDENTS_LAST_CALL': str(low_incidents_last_call),
-        'INCIDENTS_CURRENT_CALL': str(incidents_current_call),
-        'MEDIUM_INCIDENTS_CURRENT_CALL': str(medium_incidents_current_call),
-        'LOW_INCIDENTS_CURRENT_CALL': str(low_incidents_current_call)
+        'OLD_MATTERS': str(old_matters),
+        'OLD_OTHER_MATTERS': str(old_other_matters),
+        'OLD_OTHER_OTHER_MATTERS': str(old_other_other_matters),
+        'CURRENT_MATTERS': str(current_matters),
+        'OTHER_CURRENT_MATTERS': str(other_current_matters),
+        'OTHER_OTHER_CURRENT_MATTERS': str(other_other_current_matters)
     }
     update_placeholders(prs, placeholders_slide3)
     
@@ -152,9 +135,9 @@ def main():
 #################################################################################################################################################
     # Save the PowerPoint presentation with the desired filename format
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    output_folder = os.path.join(os.path.expanduser("~"), "Desktop", customer_name)
+    output_folder = os.path.join(os.path.expanduser("~"), "Desktop", subject_one)
     os.makedirs(output_folder, exist_ok=True)
-    output_filename = os.path.join(output_folder, f"SecurityCallSlides_{customer_name}_{current_date}.pptx")
+    output_filename = os.path.join(output_folder, f"Subject_{subject_one}_{current_date}.pptx")
     prs.save(output_filename)
     print(f"PowerPoint presentation '{output_filename}' created successfully.")
 
